95e259ec82b2b49512d7360a1dfcc60215c67c38
Lars-Roger Billingsø <lrb@ssb.no>
2020-09-23 10:39:15 +0200

    Modified the python code to read from event and added a template

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,9 @@ import json
 print('Loading function')
 
 def lambda_handler(event, context):
-    return event
+    servicename = event['servicename']
+    manifest = "apiVersion: sql.cnrm.cloud.google.com/v1beta1 __servicename__"
+
+    manifest = manifest.replace("__servicename__",servicename)
+    return (manifest)
     #raise Exception('Something went wrong')ß
