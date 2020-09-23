84edbcd6a46db795201062fa041a13a82ca1a08f
Lars-Roger Billingsø <lrb@ssb.no>
2020-09-23 15:52:52 +0200

    Actually replacing values

--- hello_world/app.py
+++ hello_world/app.py
@@ -5,15 +5,15 @@ import yaml
 
 
 def lambda_handler(event, context):
-  
+    print (event)
     with open("sqldatabase.yml", "r") as file:
         try:
             loaded = yaml.safe_load(file)
             print(loaded)
         except yaml.YAMLError as exc:
             print(exc)
-    loaded["metadata"]["name"] = "test"
-    loaded["metadata"]["namespace"] = "namespace_test"
+    loaded["metadata"]["name"] = event["servicename"]
+    loaded["metadata"]["namespace"] = event["namespace"]
     loaded["spec"]["instanceRef"]["name"] = loaded["metadata"]["name"] + "-sqlinstance-" + loaded["metadata"]["namespace"]
     
     return {
