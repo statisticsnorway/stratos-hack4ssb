fb5fdf15b3212a7facb31b56c0a69a18e171ac44
ArielKarlsen <aig@ssb.no>
2020-09-23 11:40:01 +0200

    Add reading and manipulating yaml file
    The python function will now read and replace strings in a yaml file.
    
    Co-authored-by: Lars-roger <lrb@ssb.no>
    Co-authored-by: Are <egk@ssb.no>

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,18 @@
 import json
+import yaml
 
 print('Loading function')
 
 def lambda_handler(event, context):
-    servicename = event['servicename']
-    manifest = "apiVersion: sql.cnrm.cloud.google.com/v1beta1 __servicename__"
 
-    manifest = manifest.replace("__servicename__",servicename)
-    return (manifest)
-    #raise Exception('Something went wrong')ß
+    with open("sqldatabase.yml", "r") as file:
+        try:
+            loaded = yaml.safe_load(file)
+            print(loaded)
+        except yaml.YAMLError as exc:
+            print(exc)
+    loaded["metadata"]["name"] = "test"
+    loaded["metadata"]["namespace"] = "namespace_test"
+    loaded["spec"]["instanceRef"]["name"] = loaded["metadata"]["name"] + "-sqlinstance-" + loaded["metadata"]["namespace"]
+    
+    return (yaml.dump(loaded, default_flow_style=False))
